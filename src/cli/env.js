9919d235bd67485d05f3853ed022f6714fba232b
Miras <miamib531@gmail.com>
2023-06-16 16:22:44 +0600

    feat: implement cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+  const prefix = 'RSS_'
+  const environmentVars = Object.entries(process.env)
+    .filter(([name]) => name.startsWith(prefix))
+    .map(([name, value]) => `${name}=${value}`)
+    .join('; ')
+  console.log(environmentVars)
+}
 
-parseEnv();
+parseEnv()
