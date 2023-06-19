0cc2eacf8dba5561a8a3e32a7c16d2fe5bb80b9b
Miras <miamib531@gmail.com>
2023-06-19 19:13:41 +0600

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import os from 'os'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { Worker } from 'worker_threads'
 
-await performCalculations();
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const fileName = path.join(__dirname, './worker.js')
+
+const performCalculations = async () => {
+  const promisesArray = Array.from({ length: os.cpus().length }, (_, i) => {
+    return new Promise(resolve => {
+      const worker = new Worker(fileName)
+      worker.on('message', data => {
+        resolve({ status: 'resolved', data })
+      })
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null })
+      })
+      worker.postMessage(i + 10)
+    })
+  })
+  const promiseResult = await Promise.allSettled(promisesArray)
+  return promiseResult.map(el => console.log(el.value))
+}
+
+await performCalculations()
